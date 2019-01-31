8db2e5132068de33d613cd0d656809c65327ce08
Ariel Brewington <ArielBrewington@outlook.com>
2019-01-31 16:52:54 -0500

    Method

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/* Ariel Brewington
+  1/31/2019
+ Demonstration of method
+ */
+ 
+using System;
 
 namespace Methods
 {
@@ -6,7 +11,30 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            Console.WriteLine("Please enter your first name.");
+            string userfirstname = Console.ReadLine();
+            Console.WriteLine("Please enter your last name.");
+            string userlastname = Console.ReadLine();
+            {
+                try
+                {
+                    
+                    Console.WriteLine("Hello" + " " + userfirstname+ " " + userlastname +'!');
+                    Console.WriteLine("Press any key to exit the program..");
+                    Console.ReadKey(true);
+
+                }
+
+                catch// end of catch
+
+                {
+                    Console.WriteLine("Please enter a letter for your first name ");
+                    Console.WriteLine("Please enter a letter for your last name ");
+                    Console.ReadKey(true);
+                }
+
+
+
+
+            } }
+    }    }
